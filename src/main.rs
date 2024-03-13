17829f8af1f135b87586225681b37fab45daae86
Noah <noah_822@163.com>
2024-03-13 22:20:31 +0800

    3rd: backward comp

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 fn main(){
@@ -8,8 +8,18 @@ fn main(){
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                let ping_response = b"+PONG\r\n+PONG\r\n";
-                let _ = stream.write_all(ping_response);
+                let mut buf = [0u8; 512];
+                let ping_response = "+PONG\r\n";
+                
+                let num_readin = stream.read(&mut buf).unwrap();
+                if num_readin < 15{
+                    stream.write_all(ping_response.clone().as_bytes())
+                            .unwrap();
+                }else{
+                    stream.write_all(ping_response.repeat(2).as_bytes())
+                            .unwrap();
+                }
+                // let _ = stream.write_all(ping_response);
             }
             Err(e) => {
                 println!("error: {}", e);
