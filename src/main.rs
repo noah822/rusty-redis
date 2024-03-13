de5ff46e1555095058958c9d282526162fbd0e18
Noah <noah_822@163.com>
2024-03-13 22:38:16 +0800

    3rd: backward comp

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,13 @@ fn main(){
         match stream {
             Ok(mut stream) => {
                 let mut buf = [0u8; 512];
-                let ping_response = "+PONG\r\n";
+                let ping_response = b"+PONG\r\n";
                 
-                let num_readin = stream.read(&mut buf).unwrap();
-                if num_readin < 15{
-                    stream.write_all(ping_response.clone().as_bytes())
-                            .unwrap();
-                }else{
-                    stream.write_all(ping_response.repeat(2).as_bytes())
-                            .unwrap();
+                loop {
+                    while stream.read(&mut buf).unwrap() == 0 {}
+                    stream.write_all(ping_response).unwrap();
                 }
-                // let _ = stream.write_all(ping_response);
+                
             }
             Err(e) => {
                 println!("error: {}", e);
